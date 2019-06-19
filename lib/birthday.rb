ed53cc189de9cfb8cc0191328e54e097163aa176
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-06-19 23:38:38 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,7 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def happy_birthday(birthday_kids)
+def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!" if age <= 12
   end
